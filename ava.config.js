529b23c026147c03e26e6883e609e939664a6ab8
Mark Wubben <mark@novemberborn.net>
2020-05-10 16:50:07 +0200

    Limit our test concurrency on Windows?

--- ava.config.js
+++ ava.config.js
@@ -4,6 +4,7 @@ if (process.versions.node < '12.16.0') {
 }
 
 export default {
+	concurrency: process.platform === 'win32' ? 1 : 0,
 	files: ['test/**', '!test/**/{fixtures,helpers}/**', ...skipTests],
 	ignoredByWatcher: ['{coverage,docs,media,test-d,test-tap}/**'],
 	timeout: '30s'
